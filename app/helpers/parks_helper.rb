820c3db668634b1770c1514200e2b568d54a3d28
qkjosh <qkjosh@gmail.com>
2020-10-16 17:56:43 -0500

    Updated the listing of parks

--- app/helpers/parks_helper.rb
+++ app/helpers/parks_helper.rb
@@ -1,2 +1,13 @@
 module ParksHelper
+  def tree_count(park_id)
+    ParkPlant.where(park_id: park_id).count
+  end
+
+  def species_count(park_id)
+    ParkPlant.where(park_id: park_id).pluck(:plant_id).uniq.count
+  end
+
+  def species(park_id)
+    ParkPlant.where(park_id: park_id).group(:plant_id)
+  end
 end
