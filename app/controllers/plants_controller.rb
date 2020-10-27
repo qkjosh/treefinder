83541907808844797b63d36ba74db518bf918d14
Joshua <jhurd59@academic.rrc.ca>
2020-10-27 00:47:39 -0500

    Added Plant search route

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -6,4 +6,9 @@ class PlantsController < ApplicationController
   def show
     @plant = Plant.find(params[:id])
   end
+
+  def search
+    @query = params[:query]
+    @plants = Plant.where('common_name LIKE ? OR scientific_name LIKE ?', "%#{@query}%", "%#{@query}%")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   resources :parks, only: %i[index show]
-  resources :plants, only: %i[index show]
   resources :genera, only: %i[index show]
   resources :families, only: %i[index show]
+
+  resources :plants, only: %i[index show] do
+    collection do
+      get 'search'
+    end
+  end
 
   get 'about', to: 'pages#about'
   root to: 'pages#home'
